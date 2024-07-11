478df97308ec12619190a6a86a8882fab9528da4
t_jmneto <ccjoao_monteiro@outlook.com>
2024-07-11 18:02:58 -0300

    tweaking the settings

--- dist/js/models/negotiation.js
+++ dist/js/models/negotiation.js
@@ -1,27 +1,37 @@
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, privateMap, value) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to set private field on non-instance");
+    }
+    privateMap.set(receiver, value);
+    return value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, privateMap) {
+    if (!privateMap.has(receiver)) {
+        throw new TypeError("attempted to get private field on non-instance");
+    }
+    return privateMap.get(receiver);
+};
+var _data, _quantity, _value;
 export class Negotiation {
-    #data;
-    #quantity;
-    #value;
-
     constructor(data, quantity, value) {
-        this.#data = data;
-        this.#quantity = quantity;
-        this.#value = value;
+        _data.set(this, void 0);
+        _quantity.set(this, void 0);
+        _value.set(this, void 0);
+        __classPrivateFieldSet(this, _data, data);
+        __classPrivateFieldSet(this, _quantity, quantity);
+        __classPrivateFieldSet(this, _value, value);
     }
-
     get data() {
-        return this.#data;
+        return __classPrivateFieldGet(this, _data);
     }
-
     get quantity() {
-        return this.#quantity;
+        return __classPrivateFieldGet(this, _quantity);
     }
-
     get value() {
-        return this.#value;
+        return __classPrivateFieldGet(this, _value);
     }
-
     get volume() {
-        return this.#quantity * this.#value;
+        return __classPrivateFieldGet(this, _quantity) * __classPrivateFieldGet(this, _value);
     }
 }
+_data = new WeakMap(), _quantity = new WeakMap(), _value = new WeakMap();
