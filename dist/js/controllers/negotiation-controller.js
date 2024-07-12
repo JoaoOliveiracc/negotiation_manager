56a3c5e7971f4c79850f0aebb9770235d5889236
t_jmneto <ccjoao_monteiro@outlook.com>
2024-07-12 12:47:09 -0300

    avoid list mutability

--- dist/js/controllers/negotiation-controller.js
+++ dist/js/controllers/negotiation-controller.js
@@ -1,10 +1,30 @@
+import { Negotiation } from "../models/negotiation.js";
+import { Negotiations } from "../models/negotiations.js";
 export class NegotiationController {
     constructor() {
+        this.negotiations = new Negotiations();
         this.inputData = document.querySelector('#data');
         this.inputQuantity = document.querySelector('#quantity');
         this.inputValue = document.querySelector('#value');
     }
     add() {
-        console.log(this.inputData, this.inputQuantity, this.inputValue);
+        const negotiation = this.createNegotiation();
+        this.negotiations.add(negotiation);
+        this.negotiations.list().pop();
+        console.log(this.negotiations.list());
+        this.clearForm();
+    }
+    createNegotiation() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantity = parseInt(this.inputQuantity.value);
+        const value = parseFloat(this.inputValue.value);
+        return new Negotiation(date, quantity, value);
+    }
+    clearForm() {
+        this.inputData.value = '';
+        this.inputQuantity.value = '';
+        this.inputValue.value = '';
+        this.inputData.focus();
     }
 }
